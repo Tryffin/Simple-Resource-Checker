df0541ad60fb2db90886dec34d40ee3933311b51
Yining BAO <yining.bao@ansys.com>
2022-04-21 15:14:24 +0200

    Add function memory usage checking

--- Check_RAM.py
+++ Check_RAM.py
@@ -1,20 +1,19 @@
-import psutil as ps
-
+import psutil, os
 
 def check_process_ram(name):
     sum = 0
-    for i in range(len(ps.pids())):
-        p = ps.Process(ps.pids()[i])
+    for i in range(len(psutil.pids())):
+        p = psutil.Process(psutil.pids()[i])
         if p.name() == name:
-            print("PID " + str(ps.pids()[i]) + " : " + name + " " +
-                  str(round(p.memory_percent() * (ps.virtual_memory()[0]/(1024**2) )/100, 2)) + 
+            print("PID " + str(psutil.pids()[i]) + " : " + name + " " +
+                  str(round(p.memory_percent() * (psutil.virtual_memory()[0]/(1024**2) )/100, 2)) + 
                     " MB")
-            sum += round(p.memory_percent() * (ps.virtual_memory()[0]/(1024**2) )/100, 2)
+            sum += round(p.memory_percent() * (psutil.virtual_memory()[0]/(1024**2) )/100, 2)
             continue
     print("Total memory use : " + str(round(sum, 2)) + " MB")
 
 def check_current_ram():
-    mem = ps.virtual_memory()
+    mem = psutil.virtual_memory()
     total = str(round(mem.total / 1024**2, 2))
     used = str(round(mem.used / 1024**2, 2))
     used_per = str(round(mem.percent))
@@ -23,4 +22,34 @@ def check_current_ram():
     print("Total used memory :" + used + " MB(" + used_per + "%)")
     print("Total available memory :" + free + " MB")
 
-print(check_current_ram())
+# inner psutil function
+def process_memory():
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    return mem_info.rss
+
+# decorator function
+def profile(func):
+    def wrapper(*args, **kwargs):
+
+        mem_before = round(process_memory()/ 1024**2, 2)
+        result = func(*args, **kwargs)
+        mem_after = round(process_memory()/ 1024**2, 2)
+        print("{}:consumed memory: {:,} MB".format(
+            func.__name__,
+            mem_before, mem_after, mem_after - mem_before))
+        
+        return result
+    return wrapper
+
+# instantiation of decorator function
+@profile
+# main code for which
+# memory has to be monitored
+def func():
+    x = [x for x in range(0, 1000)]
+    y = [y*100 for y in range(0, 1500)]
+    del x
+    return y
+
+func()
